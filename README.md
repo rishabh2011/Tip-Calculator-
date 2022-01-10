e1b5d90e41150910c80f6fd8ff59a75bc4731d22
Rishabh Choudhury <49196025+rishabh2011@users.noreply.github.com>
2022-01-10 14:58:55 +0530

    -Initial Commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Screenshot
+
+![](./Desktop.png)
+![](./Mobile.png)
+
+### Links
+
+- Solution URL: [Github](https://github.com/rishabh2011/Tip-Calculator-)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- HTML5 markup
+- CSS 
+- Bootstrap
+- JS
+
+### Useful resources
+
+- [MDN Web Docs](https://developer.mozilla.org/en-US/)
+- [Stack Overflow](https://stackoverflow.com/)
+- [Bootstrap](https://getbootstrap.com/) 
+- [JQuery](https://jquery.com/)
+
+## Author
+
+- Website - [Rishabh Choudhury](https://github.com/rishabh2011)
+- Frontend Mentor - [@rishabh2011](https://www.frontendmentor.io/profile/rishabh2011)
---
+++ js/index.js
@@ -0,0 +1,132 @@
+//---------Variables-----------
+var tipAmountText = $(".price-tip");
+var totalAmountText = $(".price-total");
+var resetBtn = $(".reset-btn");
+var customTipInput = $("input.small-box");
+
+var numPeopleInputNumbar;
+var selectedTipButton;
+
+var bill = 0;
+var numPeople = 1;
+var tipPercent = 0;
+
+var noZeroActive = false;
+var resetBtnActive = false;
+
+//---------Events------------
+
+//Tip buttons
+$("button.small-box").click(function () {
+  deselectLastTip();
+  selectedTipButton = $(this);
+  selectedTipButton.addClass("small-box-active");
+  tipPercent = Number($(this).val()) * 0.01;
+  calculatePrice();
+});
+
+//Custom Tip
+customTipInput.click(function () {
+  deselectLastTip();
+});
+
+customTipInput.on("keyup", function(){
+  tipPercent =  Math.abs(Number($(this).val())) * 0.01;
+  calculatePrice();
+})
+
+//Bill input
+$("#bill").on("keyup", function(){
+  bill = Math.abs(Number($(this).val()));
+  calculatePrice();
+});
+
+//Number of people input
+$("#num-people").on("keyup", function(){
+
+  numPeople =  Math.abs(Number($(this).val()));
+  numPeopleInputNumbar = $(this);
+
+  if(noZeroActive){
+    disableNoZero();
+  }
+
+  if(numPeople == 0){
+    $(this).addClass("num-bar-wrong");
+    $(this).css("border-color", "orange");
+    $(".no-zero").css("visibility", "visible");
+    noZeroActive = true;
+  }
+
+  calculatePrice();
+});
+
+//Reset button
+resetBtn.on("click", function(){
+
+  tipAmountText.text("$0.00");
+  totalAmountText.text("$0.00");
+
+  deselectLastTip();
+
+  $("#bill").val(null);
+  $("#num-people").val(null);
+  customTipInput.val(null);
+
+  resetBtnActive = false;
+  resetBtn.attr("disabled", "disabled");
+
+  if(noZeroActive){
+    disableNoZero();
+  }
+
+  tipPercent = 0;
+  bill = 0;
+  numPeople = 1;
+
+});
+
+//-----------Functions---------
+function disableNoZero() {
+
+  numPeopleInputNumbar.removeClass("num-bar-wrong");
+  numPeopleInputNumbar.css("border-color", "hsl(172, 67%, 45%)");
+  $(".no-zero").css("visibility", "hidden");
+  noZeroActive = false;
+
+}
+
+function deselectLastTip() {
+
+  if (selectedTipButton) {
+    selectedTipButton.removeClass("small-box-active");
+  }
+
+  customTipInput.val(null);
+
+}
+
+function calculatePrice(){
+  
+  if(numPeople != 0){
+
+    var totalTipAmount = bill * tipPercent;
+
+    var tipAmount = totalTipAmount / numPeople;
+    var totalAmount = (bill + totalTipAmount)  / numPeople;
+  
+    //Round to 2 decimal places
+    tipAmount = (Math.round((tipAmount + Number.EPSILON) * 100) / 100).toFixed(2);
+    totalAmount = (Math.round((totalAmount + Number.EPSILON) * 100) / 100).toFixed(2);
+  
+    tipAmountText.text("$" + tipAmount);
+    totalAmountText.text("$" + totalAmount);
+
+  }
+
+  if(!resetBtnActive){
+    resetBtn.removeAttr("disabled");
+    resetBtnActive = true;
+  }
+  
+}
